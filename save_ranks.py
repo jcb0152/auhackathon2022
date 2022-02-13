3b5d9abe241d8d921aea61e52dc5c487fac3f8ed
jcb0152 <jcb0152@auburn.edu>
2022-02-13 09:55:27 -0600

    Add files via upload

--- save_ranks.py
+++ save_ranks.py
@@ -1,12 +1,14 @@
 import json
 filename = "ranks.json"
 rank_list = [0,0,0,6,5,4,3,2,1] #get from application
-name = "Diet"
+
 
 ranks = {}
 with open(filename, "r") as in_file:
     ranks = json.load(in_file)
 
+print("Enter name for plan")
+name = "Diet" #placeholder
 ranks[name] = rank_list
 with open(filename, "w") as out_file:
     json.dump(ranks, out_file)
