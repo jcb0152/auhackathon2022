4c775943c9d23bcc5b89cba44126452bc994bedd
jcb0152 <jcb0152@auburn.edu>
2022-02-13 09:54:22 -0600

    Add files via upload

--- load_ranks.py
+++ load_ranks.py
@@ -1,15 +1,22 @@
-filename = "ranks.txt"
-rank_list = []
+import json
+filename = "ranks.json"
+ranks = {}
 
 with open(filename, "r") as in_file:
-    rank_list = in_file.readline()
+    ranks = json.load(in_file)
 
-rank_list = (rank_list[1:-1].split(", "))[3:]
+print("Choose saved plan")
+plans = list(ranks.keys())
+print(plans)
+name = plans[0]
+
+rank_list = ranks[name][3:]
 
 order = ["Calories", "Protein", "Carbohydrates", "Fiber", "Fat", "Saturated Fat"]
 values = {}
+
 for i in range(len(rank_list)):
     values[rank_list[i]] = order[i]
 
 for i in reversed(range(1,len(rank_list)+1)):
-    print(values[str(i)])
+    print(values[i])
--- save_ranks.py
+++ save_ranks.py
@@ -1,6 +1,14 @@
-filename = "ranks.txt"
+import json
+filename = "ranks.json"
 rank_list = [0,0,0,6,5,4,3,2,1] #get from application
+name = "Diet"
 
+ranks = {}
+with open(filename, "r") as in_file:
+    ranks = json.load(in_file)
+
+ranks[name] = rank_list
 with open(filename, "w") as out_file:
-    out_file.write(str(rank_list))
+    json.dump(ranks, out_file)
+    
 
