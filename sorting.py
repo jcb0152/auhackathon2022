a617e171335f0ef2691ee9cf508e809dda1f771b
jcb0152 <jcb0152@auburn.edu>
2022-02-12 23:27:42 -0600

    Add files via upload

--- sorting.py
+++ sorting.py
@@ -6,20 +6,23 @@ def sort(data, ranks):
     scores = []
     for i in range(len(data)):
         score = 0
-        for j in range(1, len(data[i])):
-            try:
-                score += int(data[i][j]) * int(ranks[j])
-            except:
-                continue
+        for j in range(3, len(data[i])):
+            if "," in str(data[i][j]):
+                data[i][j] = str(data[i][j])
+                data[i][j] = data[i][j].replace(",", "")
+                data[i][j] = float(data[i][j])
+            if j == 3:
+                data[i][j] = float(data[i][j]) / 10
+            score += float(data[i][j]) * float(ranks[j])
         scores.append(list((data[i][0], score)))
     return sorted(scores, key = lambda x: x[1], reverse = True)
 
 if __name__ == "__main__":
-    filename = "dataset/final_data.json"
+    filename = "dataset/fixed_data.json"
     data= {}
     with open(filename, "r") as in_file:
         data = json.load(in_file)
-        
+
     scores = [num_ranks - x for x in reversed(range(num_ranks + 1))]
     ranks = [scores[x] for x in rank_list]
 
